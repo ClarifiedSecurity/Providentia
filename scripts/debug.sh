5f11ed90ae141159b3c627d91bfac7459cc9399e
tavi.poldma <tavi.poldma@gmail.com>
2023-09-15 11:52:05 +0300

    fix debug script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -9,5 +9,5 @@ ALL_HOSTS=$(curl -k -H "Authorization: Token ${PROVIDENTIA_TOKEN}" https://${PRO
 
 # Parse the JSON array and loop over each item, making an API call per host to get the HTTP status code
 for host in $(echo $ALL_HOSTS | jq -r '.result[]'); do
-    curl -k -s -o /dev/null -I -w "${host} %{http_code}\n" -H "Authorization: Token ${PROVIDENTIA_TOKEN}" "https://${PROVIDENTIA_FQDN}/api/v3/dcm3/hosts/$host"
+    curl -k -s -o /dev/null -I -w "${host} %{http_code}\n" -H "Authorization: Token ${PROVIDENTIA_TOKEN}" "https://${PROVIDENTIA_FQDN}/api/v3/${PROVIDENTIA_PROJECT}/hosts/$host"
 done
