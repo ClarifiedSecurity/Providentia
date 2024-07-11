23994a3358c0ec8d9f11a0047b84db21f628f4a3
Mikk Romulus <mikk@clarifiedsecurity.com>
2024-07-11 09:29:38 +0300

    refactor(auth): Sign in flow changes
    * Add new column `resources` for resources returned by SSO,
      store them as returned
    * Require a default resource to be present in returned set from SSO
      to allow _any_ access

--- app/calculation/user_permissions.rb
+++ app/calculation/user_permissions.rb
@@ -1,40 +1,19 @@
 # frozen_string_literal: true
 
 class UserPermissions < Patterns::Calculation
-  ADMIN_RESOURCE_NAME = 'Admin'
+  BASIC_ACCESS = Rails.configuration.resource_prefix +
+    ENV.fetch('OIDC_RESOURCE_LOGIN', 'User')
+  SUPERADMIN_ACCESS = Rails.configuration.resource_prefix +
+    ENV.fetch('OIDC_RESOURCE_SUPERADMIN', 'Super_Admin')
+  ENVIRONMENT_CREATION = Rails.configuration.resource_prefix +
+    ENV.fetch('OIDC_RESOURCE_ENV_CREATOR', 'Environment_Creator')
 
   private
     def result
-      return if filtered_list.empty?
-
-      filtered_list.each_with_object({ admin: false }) do |resource, hash|
-        if resource == ADMIN_RESOURCE_NAME
-          hash[:admin] = true
-        else
-          Exercise.where(dev_resource_name: resource).each do |ex|
-            hash[ex.id] ||= []
-            hash[ex.id] << :gt
-          end
-
-          Exercise.where(dev_red_resource_name: resource).each do |ex|
-            hash[ex.id] ||= []
-            hash[ex.id] << :rt
-          end
-
-          Exercise.where(local_admin_resource_name: resource).each do |ex|
-            hash[ex.id] ||= []
-            hash[ex.id] << :local_admin
-          end
-        end
-      end.symbolize_keys
-    end
-
-    def filtered_list
-      (subject || []).filter_map do |resource|
-        if resource.match? resource_regex
-          resource.sub(resource_regex, '')
-        end
+      list = subject.select do |resource|
+        resource.match? resource_regex
       end
+      list if list.include?(BASIC_ACCESS)
     end
 
     def resource_regex
