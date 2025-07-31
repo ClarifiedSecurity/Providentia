7fae2b9851827d43a821669ddef37a58e681aef1
Mikk Romulus <mikk@clarifiedsecurity.com>
2025-07-31 11:54:34 +0300

    chore: rubocop fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,7 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 
 pin '@hotwired/turbo-rails', to: '@hotwired--turbo-rails.js' # @8.0.12
 pin '@hotwired/turbo', to: '@hotwired--turbo.js' # @8.0.12
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
+pin '@rails/actioncable/src', to: '@rails--actioncable--src.js' # @8.0.200
 
 pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
@@ -41,4 +41,4 @@ pin '@floating-ui/dom', to: '@floating-ui--dom.js' # @1.6.13
 pin '@floating-ui/core', to: '@floating-ui--core.js' # @1.6.9
 pin '@floating-ui/utils', to: '@floating-ui--utils.js' # @0.2.9
 pin '@floating-ui/utils/dom', to: '@floating-ui--utils--dom.js' # @0.2.9
-pin "crelt" # @1.0.6
+pin 'crelt' # @1.0.6
