9cc7a18e65ac45e4067521db4eba3735ebaebe53
Mikk Romulus <mikk@clarifiedsecurity.com>
2025-08-29 15:13:56 +0300

    chore: bump the version in quickstart script

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Download Providentia compose files from GitHub repository
 
-GITHUB_REPO="https://raw.githubusercontent.com/ClarifiedSecurity/Providentia/v25.4.0"
+GITHUB_REPO="https://raw.githubusercontent.com/ClarifiedSecurity/Providentia/v25.4.1"
 
 # Color codes (only if terminal supports it)
 if [ -t 1 ]; then
