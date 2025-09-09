fbeee7402d65ac826a519091311cc258bd252126
Mikk Romulus <mikk@clarifiedsecurity.com>
2025-09-09 16:18:05 +0300

    chore: add reload turbo action

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import "src/fonts";
+
+// custom turbo action
+Turbo.StreamActions.reload = function () {
+  // if the frame has a `src`, reload
+  // if not but has a data-src => load that src
+  // else do nothing
+  document.querySelectorAll(`turbo-frame#${this.target}`).forEach((frame) => {
+    if (frame.src) {
+      frame.reload();
+    } else if (frame.dataset.src) {
+      frame.src = frame.dataset.src;
+    }
+  });
+};
