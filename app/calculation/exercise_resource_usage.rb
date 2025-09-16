b593f7947d8563ce7ab293c63bfa8dc16cab29fc
Mikk Romulus <mikk@clarifiedsecurity.com>
2025-09-16 12:46:59 +0300

    chore(perf): optimize exercise page

--- app/calculation/exercise_resource_usage.rb
+++ app/calculation/exercise_resource_usage.rb
@@ -9,12 +9,20 @@ class ExerciseResourceUsage < Patterns::Calculation
         primary_disk_size: 0
       }
 
-      subject.virtual_machines.each_with_object(results) do |vm, results|
+      scope.each_with_object(results) do |vm, results|
         instance_count = vm.host_spec.deployable_instances.size
         results[:ram] += (vm.ram || vm.operating_system&.applied_ram).to_i * instance_count
         results[:cpu] += (vm.cpu || vm.operating_system&.applied_cpu).to_i * instance_count
         results[:primary_disk_size] += (vm.primary_disk_size || vm.operating_system&.applied_primary_disk_size).to_i * instance_count
       end
       Struct.new(*results.keys).new(**results)
+    end
+
+    def scope
+      subject.virtual_machines
+        .includes(
+          host_spec: [:virtual_machine]
+        )
+        .preload(numbered_by: [:actor])
     end
 end
