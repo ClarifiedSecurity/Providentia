94c5a595a4a000643c0557b543724f5b1d5d7f78
Mikk Romulus <mikk@clarifiedsecurity.com>
2026-05-14 15:23:48 +0300

    chore: add support for UUIDv7 primary keys

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+  primary_abstract_class
 
   nilify_blanks types: [:text, :string]
+  before_create :generate_uuid_v7
 
   def preload(*args)
     preloader = ActiveRecord::Associations::Preloader.new
     preloader.preload(self, *args)
   end
+
+  private
+    def generate_uuid_v7
+      return if self.class.attribute_types['id'].type != :uuid
+
+      self.id ||= SecureRandom.uuid_v7
+    end
 end
