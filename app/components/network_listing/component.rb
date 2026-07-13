91e12b1da11a05b7476d70f03877c9a0ea605887
Mikk Romulus <mikk@clarifiedsecurity.com>
2026-07-13 10:56:36 +0300

    fix: network listing component should cache whether it is showing actor column

--- app/components/network_listing/component.rb
+++ app/components/network_listing/component.rb
@@ -6,5 +6,8 @@ class NetworkListing::Component < ApplicationViewComponent
   option :context_actor, optional: true, model: Actor
 
   private
-    def show_actor_column? = context_actor && networks.map(&:actor).uniq != [context_actor]
+    def show_actor_column?
+      return @show_actor_column if defined?(@show_actor_column)
+      @show_actor_column = context_actor && networks.map(&:actor).uniq != [context_actor]
+    end
 end
