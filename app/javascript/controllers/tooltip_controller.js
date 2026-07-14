dae272121e27e64d5b914761ca133a94be020545
Mikk Romulus <mikk@clarifiedsecurity.com>
2026-07-14 16:26:28 +0300

    fix: tooltips persisting in DOM after turbo navigation

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -2,21 +2,29 @@ import Popover from "@stimulus-components/popover";
 import { computePosition, shift } from "@floating-ui/dom";
 
 export default class extends Popover {
+  tooltip;
+
   async show(event) {
     let el = event.currentTarget;
     let content = `<div class="floating" data-tooltip-target="card">${this.element.dataset.tooltip}</div>`;
-    let target = document.createRange().createContextualFragment(content)
+    this.tooltip = document.createRange().createContextualFragment(content)
       .children[0];
-    computePosition(el, target, {
+    computePosition(el, this.tooltip, {
       placement: "top",
       middleware: [shift()],
     }).then(({ x, y }) => {
-      Object.assign(target.style, {
+      Object.assign(this.tooltip.style, {
         left: `${x}px`,
         top: `${y}px`,
       });
     });
 
-    this.element.append(target);
+    this.element.append(this.tooltip);
+  }
+
+  disconnect() {
+    if (this.tooltip) {
+      this.tooltip.remove();
+    }
   }
 }
